13a6744503d8b86c58d0b0cdc90f4f73350f4f17
Tanuj <101932530+Tkundlas@users.noreply.github.com>
2023-03-08 10:20:05 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,20 +1,37 @@
 #include<vector>
+#include<iostream>
 using namespace std;
-void bubbleSort(vector<int>& arr, int n)
+
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void bubbleSort(int arr[], int n)
 {   
-    for(int i = 1; i<n; i++) {
-        bool swapped = false;
-        
-        for(int j =0; j<n-i; j++) {
+    for(int i = 1; i<n; i++){
+        for(int j =0; j<n-i; j++){
             if(arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j+1]);
-                swapped = true;
             }
-            
-        }
-        if(swapped == false){
-            break;
         }
            
     }
+}
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout<< array[i] << " ";
+  }
+  cout << endl;
+} 
+
+int main()
+{
+    int data[] = {20, 12, 10, 15, 2};
+  int size = sizeof(data) / sizeof(data[0]);
+  bubbleSort(data, size);
+  cout<< "Sorted array in Acsending Order:\n";
+  printArray(data, size);
 }
